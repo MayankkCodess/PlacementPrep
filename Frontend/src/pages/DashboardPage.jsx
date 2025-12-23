d644a12de020ba73442353e4d0e0627f377b39de
Mayank Kaushik <kaushmayank218@gmail.com>
2025-12-23 20:15:59 +0530

    23/12/ work

--- Frontend/src/pages/DashboardPage.jsx
+++ Frontend/src/pages/DashboardPage.jsx
@@ -16,13 +16,12 @@ import {
   AvatarImage,
 } from "@/components/ui/avatar.jsx";
 import { ChevronDown } from "lucide-react";
-import {Plus} from "lucide-react";
+import { Plus } from "lucide-react";
 
 const DashboardPage = () => {
   return (
     <div className="w-full flex justify-center">
       <div className="w-[70%]">
-
         {/* Top Navbar For Dashboard Page */}
 
         <div className="navbar flex ">
@@ -73,23 +72,29 @@ const DashboardPage = () => {
 
         {/* Statistics Cards For Dashboard Page*/}
 
-          <div className="flex justify-between">
-            <div className="w-72 rounded-md bg-[#18252C]">
-              <p className="text-[#9DB0B9] text-sm">Recent Interviews</p>
-              <h1 className="text-[#E5E7EB]" >5</h1>
-              <a href="" className="text-[#53A5ED] text-xs">View All</a>
-            </div>
-            <div className="w-72 rounded-md bg-[#18252C]">
-              <p className="text-[#9DB0B9] text-sm" >Upcoming Mock</p>
-              <h1 className="text-[#E5E7EB] font-extrabold" >Dec 15,2023</h1>
-              <a href=""  className="text-[#53A5ED] text-xs">View Schedule</a>
-            </div>
-            <div className="w-72 h-32 rounded-md bg-[#18252C] ">
-              <p className="text-[#9DB0B9] text-sm">Latest Score</p>
-              <h1 className="text-[#E5E7EB]">89%</h1>
-              <a href="" className="text-[#53A5ED] text-xs">View Report</a>
-            </div>
+        <div className="flex justify-between">
+          <div className="w-72 rounded-md bg-[#18252C]">
+            <p className="text-[#9DB0B9] text-sm">Recent Interviews</p>
+            <h1 className="text-[#E5E7EB]">5</h1>
+            <a href="" className="text-[#53A5ED] text-xs">
+              View All
+            </a>
           </div>
+          <div className="w-72 rounded-md bg-[#18252C]">
+            <p className="text-[#9DB0B9] text-sm">Upcoming Mock</p>
+            <h1 className="text-[#E5E7EB] font-extrabold">Dec 15,2023</h1>
+            <a href="" className="text-[#53A5ED] text-xs">
+              View Schedule
+            </a>
+          </div>
+          <div className="w-72 h-32 rounded-md bg-[#18252C] ">
+            <p className="text-[#9DB0B9] text-sm">Latest Score</p>
+            <h1 className="text-[#E5E7EB]">89%</h1>
+            <a href="" className="text-[#53A5ED] text-xs">
+              View Report
+            </a>
+          </div>
+        </div>
 
         {/* Interview Filters & Cards For Dashboard Page */}
 
@@ -102,72 +107,72 @@ const DashboardPage = () => {
                 {/* Filter 1 */}
                 <div>
                   <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
-                    >
-                      Role
-                      <ChevronDown className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="secondary"
+                        size="sm"
+                        className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
+                      >
+                        Role
+                        <ChevronDown className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
 
-                  <DropdownMenuContent align="start">
-                    <DropdownMenuItem>Software Engineer</DropdownMenuItem>
-                    <DropdownMenuItem>Product Manager</DropdownMenuItem>
-                    <DropdownMenuItem>Data Analyst</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                    <DropdownMenuContent align="start">
+                      <DropdownMenuItem>Software Engineer</DropdownMenuItem>
+                      <DropdownMenuItem>Product Manager</DropdownMenuItem>
+                      <DropdownMenuItem>Data Analyst</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
 
                 {/* Filter 2 */}
                 <div>
                   <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
-                    >
-                      Date Range
-                      <ChevronDown className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="secondary"
+                        size="sm"
+                        className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
+                      >
+                        Date Range
+                        <ChevronDown className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
 
-                  <DropdownMenuContent align="start">
-                    <DropdownMenuItem>Software Engineer</DropdownMenuItem>
-                    <DropdownMenuItem>Product Manager</DropdownMenuItem>
-                    <DropdownMenuItem>Data Analyst</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                    <DropdownMenuContent align="start">
+                      <DropdownMenuItem>Software Engineer</DropdownMenuItem>
+                      <DropdownMenuItem>Product Manager</DropdownMenuItem>
+                      <DropdownMenuItem>Data Analyst</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
                 {/* Filter 3 */}
                 <div>
                   <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
-                    >
-                      Package
-                      <ChevronDown className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="secondary"
+                        size="sm"
+                        className="flex text-[#9DB0B9] bg-[#18252C] items-center gap-2 rounded-md"
+                      >
+                        Package
+                        <ChevronDown className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
 
-                  <DropdownMenuContent align="start">
-                    <DropdownMenuItem>Software Engineer</DropdownMenuItem>
-                    <DropdownMenuItem>Product Manager</DropdownMenuItem>
-                    <DropdownMenuItem>Data Analyst</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                    <DropdownMenuContent align="start">
+                      <DropdownMenuItem>Software Engineer</DropdownMenuItem>
+                      <DropdownMenuItem>Product Manager</DropdownMenuItem>
+                      <DropdownMenuItem>Data Analyst</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
               </div>
             </div>
             <div>
               <Button className="flex justify-evenly px-4 py-4 bg-linear-to-r from-[#DE7478] to-[#8593DE]">
-                <Plus/>
+                <Plus />
                 Add Interview
               </Button>
             </div>
@@ -175,63 +180,86 @@ const DashboardPage = () => {
           {/* Section 2 */}
           <div className="">
             {/* main for cards  */}
-            <div>
-              <div>
-                <img className="w-50 h-34" src="/Fullstack.jpg" alt="Interview Card Image" />
-                <h3>Software Engineer Intern</h3>
-                <p>Google</p>
+            <div className="flex">
+              <div className="w-[236px] h-[294px] rounded-lg flex justify-center items-center bg-[#18252C]">
                 <div>
-                  <p>15 Dec 2023</p>
-                <p>&#x20B9;12 LPA</p>
-                </div>
-                <div>
-                  <span>On-Campus</span>
-                  <span>SDE</span>
+                    <img
+                    className="w-50 h-34"
+                    src="/Fullstack.jpg"
+                    alt="Interview Card Image"
+                  />
+                  <h3 className="text-[#E5E7EB]">Software Engineer Intern</h3>
+                  <p className="text-[#9DB0B9] text-sm">Google</p>
+                  <div className="flex gap-4">
+                    <p className="text-[#9DB0B9]">15 Dec 2023</p>
+                    <p className="text-[#9DB0B9]">&#x20B9;12 LPA</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <span className="border-black p-4 text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">On-Campus</span>
+                    <span className="border-black text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">SDE</span>
+                  </div>
                 </div>
               </div>
               {/* Card - 2 */}
-              <div>
-                <img className="w-50 h-34" src="/ProductMan.jpg" alt="Interview Card Image" />
-                <h3>Software Engineer Intern</h3>
-                <p>Google</p>
+               <div className="w-[236px] h-[294px] rounded-lg flex justify-center items-center bg-[#18252C]">
                 <div>
-                  <p>15 Dec 2023</p>
-                <p>&#x20B9;12 LPA</p>
-                </div>
-                <div>
-                  <span>On-Campus</span>
-                  <span>SDE</span>
+                    <img
+                    className="w-50 h-34"
+                    src="/Fullstack.jpg"
+                    alt="Interview Card Image"
+                  />
+                  <h3 className="text-[#E5E7EB]">Software Engineer Intern</h3>
+                  <p className="text-[#9DB0B9] text-sm">Google</p>
+                  <div className="flex gap-4">
+                    <p className="text-[#9DB0B9]">15 Dec 2023</p>
+                    <p className="text-[#9DB0B9]">&#x20B9;12 LPA</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <span className="border-black p-4 text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">On-Campus</span>
+                    <span className="border-black text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">SDE</span>
+                  </div>
                 </div>
               </div>
               {/* Card - 3 */}
-              <div>
-                <img className="w-50 h-34" src="/data analysis.jpg" alt="Interview Card Image" />
-                <h3>Software Engineer Intern</h3>
-                <p>Google</p>
+               <div className="w-[236px] h-[294px] rounded-lg flex justify-center items-center bg-[#18252C]">
                 <div>
-                  <p>15 Dec 2023</p>
-                <p>&#x20B9;12 LPA</p>
-                </div>
-                <div>
-                  <span>On-Campus</span>
-                  <span>SDE</span>
+                    <img
+                    className="w-50 h-34"
+                    src="/Fullstack.jpg"
+                    alt="Interview Card Image"
+                  />
+                  <h3 className="text-[#E5E7EB]">Software Engineer Intern</h3>
+                  <p className="text-[#9DB0B9] text-sm">Google</p>
+                  <div className="flex gap-4">
+                    <p className="text-[#9DB0B9]">15 Dec 2023</p>
+                    <p className="text-[#9DB0B9]">&#x20B9;12 LPA</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <span className="border-black p-4 text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">On-Campus</span>
+                    <span className="border-black text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">SDE</span>
+                  </div>
                 </div>
               </div>
               {/* Card - 4 */}
-              <div>
-                <img className="w-50 h-34" src="/uiux.jpg" alt="Interview Card Image" />
-                <h3>Software Engineer Intern</h3>
-                <p>Google</p>
+               <div className="w-[236px] h-[294px] rounded-lg flex justify-center items-center bg-[#18252C]">
                 <div>
-                  <p>15 Dec 2023</p>
-                <p>&#x20B9;12 LPA</p>
-                </div>
-                <div>
-                  <span>On-Campus</span>
-                  <span>SDE</span>
+                    <img
+                    className="w-50 h-34"
+                    src="/Fullstack.jpg"
+                    alt="Interview Card Image"
+                  />
+                  <h3 className="text-[#E5E7EB]">Software Engineer Intern</h3>
+                  <p className="text-[#9DB0B9] text-sm">Google</p>
+                  <div className="flex gap-4">
+                    <p className="text-[#9DB0B9]">15 Dec 2023</p>
+                    <p className="text-[#9DB0B9]">&#x20B9;12 LPA</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <span className="border-black p-4 text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">On-Campus</span>
+                    <span className="border-black text-sm bg-[#1B3E52] rounded-full text-[#53A5ED] justify-center items">SDE</span>
+                  </div>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
